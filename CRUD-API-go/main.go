63c65de1d5d7f7343e9f9011eff5d435867ad36c
madhur-banger <madhur.cloudevops@gmail.com>
2024-10-16 22:23:01 +0530

    Added PUT

--- CRUD-API-go/main.go
+++ CRUD-API-go/main.go
@@ -72,7 +72,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[:index+1]...)
 			var movie Movie
-			_ = json.NewDecoder(r.Body).Decode(movie)
+			_ = json.NewDecoder(r.Body).Decode(&movie)
 			movie.ID = params["id"]
 			movies = append(movies, movie)
 			json.NewEncoder(w).Encode(movie)
@@ -92,7 +92,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("UPDATE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
 
 	fmt.Println("Starting Server at Port : 8000")
